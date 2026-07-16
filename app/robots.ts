8be729a205eae613e9dcd6b1f57f7539160d6f68
Blackmafia453 <kingadi2328@gmail.com>
2026-07-16 18:07:39 +0500

    Add robots.txt for SEO

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap:
+      "https://toolverse-topaz-nu.vercel.app/sitemap.xml",
+  };
+}
